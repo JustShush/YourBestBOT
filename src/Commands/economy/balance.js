607574e77257945210862d1fce80443131803f4b
JustShush <marquesdinis.p@gmail.com>
2023-04-03 22:38:48 +0100

    just creting the new branch

--- src/Commands/economy/balance.js
+++ src/Commands/economy/balance.js
@@ -19,7 +19,7 @@ module.exports = {
 	async execute(interaction, client) {
 		try {
 
-			// random colors from one dark color palette 
+			// random colors from one dark color palette
 			const colors = ['#282C34', '#E06C75', '#98C379', '#E5C07b', '#61AFEF', '#C678DD', '#56B6C2', '#ABB2BF', '#6B859E', '#3890E9', '#A359ED', '#EC5252', '#C97016', '#5DA713', '#13AFAF'];
 			const color = Math.floor(Math.random() * colors.length);
 			const resColor = colors[color];

981115be4144188cb6958540ad3cf843d0dc3fd1
JustShushVPS <marquesdinis.p@gmail.com>
2024-08-20 13:08:26 +0000

    Better clear command again
    - Now not only you can delete more then 100 msgs at once but also;
    - You can delete msgs from a specific user, its a new option

--- src/Commands/moderation/clear.js
+++ src/Commands/moderation/clear.js
@@ -15,6 +15,10 @@ module.exports = {
 			.setName("amount")
 			.setDescription("The amount of messages to delete.(max 100)")
 			.setRequired(true))
+		.addUserOption((option) => option
+			.setName('user')
+			.setDescription('Clear messages of a specific user')
+			.setRequired(false))
 		.setDMPermission(false)
 		.setNSFW(false),
 	/**
@@ -26,13 +30,14 @@ module.exports = {
 			const { options, guild } = interaction
 
 			const n = options.getString("amount");
+			const user = options.getUser('user');
 
 			if (isNaN(n)) return interaction.reply({ content: 'Please provide a valid number between 1 and 100.', ephemeral: true });
 			if (n < 1) return interaction.reply({ content: "You have to delete at least 1 message!", ephemeral: true });
-			
+
 			await interaction.deferReply({ ephemeral: true });
 
-			deleteMessages(interaction.channel, parseInt(n));
+			deleteMessages(interaction.channel, parseInt(n), user);
 
 			const logchannel = await logdb.findOne({ Guild: guild.id })
 			if (logchannel) {
@@ -51,8 +56,21 @@ module.exports = {
 	}
 }
 
-async function deleteMessages(channel, totalToDelete) {
+async function deleteMessages(channel, totalToDelete, user) {
 	let remaining = totalToDelete;
+	if (user) {
+		let messages;
+		while (remaining > 0) {
+			messages = await channel.messages.fetch()
+				.then(messages => messages.filter(m => m.author.id === user.id))
+				.then(messages => messages.first(parseInt(remaining)));
+			const deleteCount = remaining > 100 ? 100 : remaining;
+			// the true here forces the bot to delete msgs that older then 14 days.
+			await channel.bulkDelete(messages, true);
+			remaining -= deleteCount;
+		}
+		return;
+	}
 	while (remaining > 0) {
 		const deleteCount = remaining > 100 ? 100 : remaining;
 		// the true here forces the bot to delete msgs that older then 14 days.

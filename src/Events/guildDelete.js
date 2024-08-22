e0cca0529e9627601fb7dc3c4f5745ac417871eb
JustShushVPS <marquesdinis.p@gmail.com>
2024-08-22 10:38:31 +0000

    fixed leave noti

--- src/Events/guildDelete.js
+++ src/Events/guildDelete.js
@@ -19,26 +19,28 @@ module.exports = {
 		}
 		await data.save();
 
-		/* let found = guild.channels.cache.find(
+		let found = guild.channels.cache.find(
 			(channel) => channel.type === ChannelType.GuildText && channel.permissionsFor(guild.members.me).has("SEND_MESSAGES")
 		);
-		const invite = await found.createInvite({
-			maxAge: 0,
-			maxUses: 0,
-		}) */
+		if (found) {
+			const invite = await found.createInvite({
+				maxAge: 0,
+				maxUses: 0,
+			}).catch((err) => console.log(err));
+		}
 
 		const channel = client.channels.cache.get(client.config.config.logs[0].id);
 		const newEmbed = new EmbedBuilder()
 			.setAuthor({ name: `Just got kicked from a server!` })
 			.setDescription(`GuildName: \`${guild.name}\` | id: \`${guild.id}\`\nOwner: <@!${guild.ownerId}> | ${guild.ownerId}`)
-			.addFields({ name: `Number of members:`, value: `${guild.memberCount}` })
+			.addFields({ name: `Number of members:`, value: `${guild.memberCount}`, inline: false })
 			.setColor("Red")
 			.setThumbnail(guild.iconURL())
 			.setTimestamp();
 
-		newEmbed.addFields({ name: `Couldn\'t create the invite.` })
+		//newEmbed.addFields({ name: `Couldn\'t create the invite.`, value: "." })
 		//newEmbed.addFields({ name: `Invite:`, value: `\`discord.gg/${invite.code}\`` })
-		//if (found) channel.send({ content: `discord.gg/${invite.code}`, embeds: [newEmbed] });
-		channel.send({ embeds: {newEmbed} });
+		if (found) return channel.send({ content: `discord.gg/${invite.code}`, embeds: [newEmbed] });
+		channel.send({ embeds: [newEmbed] });
 	},
 };

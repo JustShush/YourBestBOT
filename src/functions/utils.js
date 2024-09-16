d15cd57e9ee9c446fa763c76f14a966f6b81ae02
JustShushVPS <marquesdinis.p@gmail.com>
2024-09-16 18:04:12 +0000

    updating the timestamp function so it gives me the correct time

--- src/functions/utils.js
+++ src/functions/utils.js
@@ -85,12 +85,12 @@ async function getTopServers(client, nbr) {
 
 function getTimestamp() {
 	const date = new Date();
-	const year = date.getFullYear();
-	const month = date.getMonth() + 1;
-	const day = date.getDate();
-	const hours = date.getHours();
-	const minutes = date.getMinutes();
-	const seconds = date.getSeconds();
+	const year = date.getUTCFullYear();
+	const month = date.getUTCMonth() + 1;
+	const day = date.getUTCDate();
+	const hours = date.getUTCHours();
+	const minutes = date.getUTCMinutes();
+	const seconds = date.getUTCSeconds();
 	return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 

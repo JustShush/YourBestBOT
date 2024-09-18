9fa90b1a024f50ddfc045a2cfebcb7284171c45a
JustShushVPS <marquesdinis.p@gmail.com>
2024-09-18 21:03:06 +0000

    fixing the time

--- src/functions/utils.js
+++ src/functions/utils.js
@@ -88,7 +88,7 @@ function getTimestamp() {
 	const year = date.getUTCFullYear();
 	const month = date.getUTCMonth() + 1;
 	const day = date.getUTCDate();
-	const hours = date.getUTCHours();
+	const hours = date.getUTCHours() + 1;
 	const minutes = date.getUTCMinutes();
 	const seconds = date.getUTCSeconds();
 	return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;

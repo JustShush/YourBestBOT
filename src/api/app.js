77952b9dac3acb97056ed854465d2960998463ce
JustShushVPS <marquesdinis.p@gmail.com>
2024-10-08 17:49:26 +0000

    Added rate Limit to requests, no special reason

--- src/api/app.js
+++ src/api/app.js
@@ -1,9 +1,21 @@
 // https://api.yourbestbot.pt
 const color = require('colors');
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const app = express();
 const { getTimestamp } = require("../functions/utils.js");
 const port = process.env.PORT || 80
+
+// Define the rate limiter
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 5, // Limit each IP to 100 requests per `window` (15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware globally
+app.use(limiter);
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');

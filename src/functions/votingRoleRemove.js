c4770d22806c1a5a1bcf5ccb024c58f4abd882fe
JustShushVPS <marquesdinis.p@gmail.com>
2024-10-11 18:07:05 +0000

    fixed voting remove

--- src/functions/votingRoleRemove.js
+++ src/functions/votingRoleRemove.js
@@ -2,7 +2,7 @@ const colors = require('colors');
 const Schema = require("../schemas/votes.js");
 const UserStatsSchema = require('../schemas/userStats.js');
 const { INFO, getTimestamp } = require("./utils.js");
-const config = require("../../config.json");
+const Config = require("../../config.json");
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 async function RVotingRole(client) {
@@ -11,22 +11,20 @@ async function RVotingRole(client) {
 	if (!data || data.length === 0) return console.log("No data found");
 	console.log(colors.yellow(`[${getTimestamp()}]`), `${data.length} Active Voters!`);
 
-	const ybb = config.config.votes.support.guildId; // YBB server Id
-	const ybbR = config.config.votes.support.roleId; // YBB Voting role Id
+	const ybb = Config.config.votes.support.guildId; // YBB server Id
+	const ybbR = Config.config.votes.support.roleId; // YBB Voting role Id
 	if (!client.guilds.cache.size) await client.guilds.fetch().catch((err) => console.log(err));
 	const ybbS = await client.guilds.cache.get(ybb);
 	if (!ybbS) return console.log('Can\'t find the ybb server to remove vote role');
-
+	
 	data.forEach(async (user) => {
 		if (Date.now() - user.last >= timer) {
 			try {
-				console.log(user);
 				if (!ybbS.members.cache.size) await ybbS.members.fetch();
-				console.log(ybbS.members.cache[0]);
-				const ybbMember = await ybbS.members.cache.get(user.UserId);
-				console.log(ybbMember)
-				//? DM the member with the remainder to vote again!
+				let ybbMember = await ybbS.members.cache.get(user.UserId);
+				if (!ybbMember) ybbMember = await ybbS.members.fetch(user.UserId);
 
+				//? DM the member with the remainder to vote again!
 				const newEmbed = new EmbedBuilder()
 					.setTitle(`<3 Yoo, ${ybbMember.user.globalName}`)
 					.setDescription(`<:dot:1289304871467483216> **Looks like you can vote again for me on top.gg! \<3**\n<:dot:1289304871467483216> Vote again to keep awesome perks such as a [voter role in our community server](${client.config.topgg}) and more..\n\nThank you for voting \<3`)
@@ -37,7 +35,7 @@ async function RVotingRole(client) {
 						new ButtonBuilder()
 							.setLabel("Vote for YourBestBot")
 							.setStyle(ButtonStyle.Link)
-							.setURL(`${config.topgg}`)
+							.setURL(`${Config.topgg}`)
 					)
 
 				await ybbMember.send({ embeds: [newEmbed], components: [row] });

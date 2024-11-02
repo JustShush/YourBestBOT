8cd820fcd5b09e7ef67d6b1ec083359f008ee899
JustShushVPS <marquesdinis.p@gmail.com>
2024-11-02 23:29:35 +0000

    Removed unused parameters from DJS

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 const colors = require("colors");
-const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+const { REST, Routes } = require('discord.js');
 const { clientId, TOKEN } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');

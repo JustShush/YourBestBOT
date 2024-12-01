f211f04c82fb2cef1a2a4faadda34329480c5b54
Wie <alex1061.business@gmail.com>
2024-12-01 13:14:23 +0100

    Create main.js

---
+++ src/api/main.js
@@ -0,0 +1,13 @@
+// Author https://github.com/justshush
+const { Client } = require("discord.js");
+
+/**
+ * @typedef {import('express').Request} CustomRequest
+ * @typedef {import('express').Response} CustomResponse
+ * @param {CustomRequest} req
+ * @param {CustomResponse} res
+ * @param {Client} client
+ */
+module.exports = (req, res, client) => {
+	res.status(404).send({ message: `Mhm, it seems I couldn't find this route.` });
+}

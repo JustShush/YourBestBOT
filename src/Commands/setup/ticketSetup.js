c9691c6de1452f0ddef7c81556da7cb6e0aa3a23
JustShushVPS <marquesdinis.p@gmail.com>
2025-01-19 12:39:48 +0000

    Now you can remove a role from the ticket

--- src/Commands/setup/ticketSetup.js
+++ src/Commands/setup/ticketSetup.js
@@ -157,11 +157,13 @@ module.exports = {
 				if (!ticketData) return await interaction.reply({ content: `Looks like you don\'t have the ticket system setup in this server.\nUse </ticket setup:1305164630213267487> to set it up \<3`, ephemeral: true });
 
 				user = await options.getUser('user');
+				role = await options.getRole('role');
+				if (!user && !role) return interaction.reply({ content: `You need to provide a user or a role to remove from the ticket.`, flags:'ephemeral' });
 
 				channel = interaction.channel;
 				if (channel.parentId !== ticketData.CategoryId) return await interaction.reply({ content: `Sorry but you aren\'t using the command in a ticket channel, you can only do that inside Category: \`${ticketData.CategoryId}\` | "<#${ticketData.CategoryId}>"`, ephemeral: true });
 
-				await channel.permissionOverwrites.edit(user.id, {
+				await channel.permissionOverwrites.edit(user ? user.id : role.id, {
 					ViewChannel: false,
 					SendMessages: false,
 					AttachFiles: false,
@@ -174,7 +176,7 @@ module.exports = {
 				}).catch(err => console.log(err));
 
 				newEmbed = new EmbedBuilder()
-					.setDescription(`<@${user.id}> removed from the ticket ${channel}`)
+					.setDescription(`<${user ? `@${user.id}` : `@&${role.id}`}> removed from the ticket ${channel}`)
 					.setColor('Green')
 
 				await interaction.reply({ embeds: [newEmbed] });
